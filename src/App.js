8d6c61a4256cf255e90dee1a829d1a0640fb146a
dsyscms <dsyscms@gmail.com>
2023-05-05 05:33:55 +0800

    add artplayer support

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Artplayer from "./ArtPlayer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+            <div>
+
+                <Artplayer
+                    option={{
+                    }}
+                    style={{
+                        width: '600px',
+                        height: '400px',
+                        margin: '60px auto 0',
+                    }}
+                    getInstance={(art) => console.info(art)}
+                />
+            </div>
+        </>
+    );
 }
 
 export default App;
